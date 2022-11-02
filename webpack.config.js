28ba36be516952afdd056c238207c6e360310505
katayama <tattu.0310@gmail.com>
2022-11-02 18:43:06 +0900

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/food-app/main.ts',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/'
     },
     devtool: 'inline-source-map',
     module: {
